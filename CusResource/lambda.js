b8ebe26e59fabdba2cc5537a5e6dc52cbd2688b3
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-30 14:39:33 +0530

    add SNS

--- CusResource/lambda.js
+++ CusResource/lambda.js
@@ -1,5 +1,17 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = function (event, context, callback) {
     //hirudinee
+    sns.getTopicAttributes({
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
     //kkk
     callback(null, { "message": "Successfully executed" });
 }
