9196869b4fc7a4bbb4482ad4939f802ca1c4b038
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-30 14:40:26 +0530

    change same line

--- CusResource/lambda.js
+++ CusResource/lambda.js
@@ -1,17 +1,5 @@
-let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-
 exports.handler = function (event, context, callback) {
-    //hirudinee
-    sns.getTopicAttributes({
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    //hirudinee edit
     //kkk
     callback(null, { "message": "Successfully executed" });
 }
