1d2f3af5ffdde46ac87ecb700f16193f90993b28
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-08-30 14:09:10 +0530

    remove resource

--- CusResource/lambda.js
+++ CusResource/lambda.js
@@ -1,18 +1,4 @@
-let AWS = require('aws-sdk');
-const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-
 exports.handler = function (event, context, callback) {
-    cognito_idp.listUsers({
-        UserPoolId: "us-east-1_D10y3fy0o",
-        AttributesToGet: ["sub", "name"],
-        Limit: 100
-    }, function (error, data) {
-        if (error) {
-            // implement error handling logic here
-            throw error;
-        }
-        // your logic goes within this block
-    });
 
     callback(null, { "message": "Successfully executed" });
 }
